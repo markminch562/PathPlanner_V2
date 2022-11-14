dc72377d045a3a0f87b09891909dd0411a4512c6
Mark <markminch562@gmail.com>
2022-11-13 22:19:13 -0500

    Basic Starting files build paths for the project

---
+++ src/executables/openWindow.cpp
@@ -0,0 +1,12 @@
+//
+// Created by plotb on 11/13/22.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout<<"A window will be created here for this project"<<std::endl;
+    return 0;
+}
